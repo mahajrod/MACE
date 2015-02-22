8cd2f63b503fd6a543596f0e13f6f70744c8e6c9
Sergei Kliver <mahajrod@gmail.com>
2015-02-22 23:12:10 +0300

    a

---
+++ MCT/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-c", "", action="store", dest="",
+                    help="")
+parser.add_argument("-c", "", action="store", dest="",
+                    help="")
+parser.add_argument("-c", "", action="store", dest="",
+                    help="")
+parser.add_argument("-c", "", action="store", dest="",
+                    help="")
+parser.add_argument("-c", "", action="store", dest="",
+                    help="")
+parser.add_argument("-c", "", action="store", dest="",
+                    help="")
+args = parser.parse_args()
