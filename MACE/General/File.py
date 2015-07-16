56f13be099485b24422fd4b4d2ac174a6ac00a0a
Sergei Kliver <mahajrod@gmail.com>
2015-07-17 00:29:59 +0200

    added vcf_length.py

---
+++ MACE/General/File.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+
+
+def split_filename(filepath):
+    directory, basename = os.path.split(filepath)
+    prefix, extension = os.path.splitext(basename)
+    return directory, prefix, extension
+
+
+def make_list_of_path_to_files(list_of_dirs_and_files, expression):
+
+    pathes_list = []
+    for entry in list_of_dirs_and_files:
+        if os.path.isdir(entry):
+            files_in_dir = filter(expression, os.listdir(entry))
+            for filename in files_in_dir:
+                pathes_list.append("%s/%s" % (entry, filename))
+        elif os.path.exists(entry):
+            pathes_list.append(entry)
+        else:
+            print("%s does not exist" % entry)
+
+    return pathes_list
