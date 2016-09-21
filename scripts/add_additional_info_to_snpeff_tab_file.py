1eb64c98780e9de1d767c6625d0b7b9d5338e94e
mahajrod <mahajrod@gmail.com>
2016-09-21 14:55:39 +0300

    added  set_repeat_filters

--- scripts/add_additional_info_to_snpeff_tab_file.py
+++ scripts/add_additional_info_to_snpeff_tab_file.py
@@ -2,6 +2,7 @@
 __author__ = 'Sergei F. Kliver'
 import argparse
 
+from MACE.General.GeneralCollections import SynDict
 from MACE.Parsers.SNPeff import CollectionSNPeff
 
 
@@ -12,9 +13,49 @@ parser.add_argument("-i", "--input_snpeff", action="store", dest="input_snpeff",
 parser.add_argument("-o", "--output_snpeff", action="store", dest="output_snpeff", required=True,
                     help="Output snpeff file")
 
+parser.add_argument("-a", "--alias_file", action="store", dest="alias_file",
+                    help="File with gene name  aliases")
+parser.add_argument("--alias_key", action="store", dest="alias_key", type=int, default=0,
+                    help="Column with gene names in alias file (0-based). Default - 0")
+parser.add_argument("--alias_value", action="store", dest="alias_value", type=int, default=1,
+                    help="Column with gene aliases in alias file (0-based). Default - 1")
+
+parser.add_argument("-f", "--function_file", action="store", dest="function_file",
+                    help="File with gene functions")
+parser.add_argument("--function_key", action="store", dest="function_key", type=int, default=0,
+                    help="Column with gene names in function file (0-based). Default - 0")
+parser.add_argument("--function_value", action="store", dest="function_value", type=int, default=1,
+                    help="Column with gene functions in function file (0-based). Default - 1")
+
+parser.add_argument("-d", "--description_file", action="store", dest="description_file",
+                    help="File with descriptions")
+parser.add_argument("--description_key", action="store", dest="description_key", type=int, default=0,
+                    help="Column with gene names in description file (0-based). Default - 0")
+parser.add_argument("--description_value", action="store", dest="description_value", type=int, default=1,
+                    help="Column with description in description file (0-based). Default - 1")
+
 args = parser.parse_args()
 
 snpeff_collection = CollectionSNPeff(input_file=args.input_snpeff, from_file=True, filetype='tab')
+
+if args.alias_file:
+    alias_dict = SynDict()
+    alias_dict.read(args.alias_file, split_values=True, key_index=args.alias_key, value_index=args.alias_value)
+    snpeff_collection.add_gene_name_aliases(alias_dict)
+
+if args.function_file:
+    function_dict = SynDict()
+    function_dict.read(args.function_file, split_values=True, key_index=args.function_key,
+                       value_index=args.function_value)
+    snpeff_collection.add_gene_functions(function_dict)
+
+if args.description_file:
+    description_dict = SynDict()
+    description_dict.read(args.description_file, split_values=True, key_index=args.description_key,
+                          value_index=args.description_value)
+    snpeff_collection.add_gene_name_aliases(description_dict)
+
 snpeff_collection.write(args.output_snpeff)
+
 
 
