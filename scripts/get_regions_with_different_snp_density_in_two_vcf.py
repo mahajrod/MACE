63da8df60577f1f5f5f43527f1dc37d2d4100263
mahajrod <mahajrod@gmail.com>
2017-11-17 16:11:41 +0300

    l

--- scripts/get_regions_with_different_snp_density_in_two_vcf.py
+++ scripts/get_regions_with_different_snp_density_in_two_vcf.py
@@ -32,7 +32,7 @@ parser.add_argument("-s", "--window_step", action="store", dest="window_step", d
 parser.add_argument("-p", "--parsing_mode", action="store", dest="parsing_mode", default="index_db",
                     help="Parsing mode for input sequence file. "
                          "Possible variants: 'index_db'(default), 'index', 'parse'")
-parser.add_argument("-a", "--scaffold_white_list", action="store", dest="scaffold_white_list", default=[],
+parser.add_argument("-x", "--scaffold_white_list", action="store", dest="scaffold_white_list", default=[],
                     type=lambda s: s.split(","),
                     help="Comma-separated list of the only scaffolds to draw. Default: all")
 parser.add_argument("-b", "--scaffold_black_list", action="store", dest="scaffold_black_list", default=[],
