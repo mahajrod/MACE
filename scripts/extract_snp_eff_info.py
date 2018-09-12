1a1b8c05eba1f1f58a6f6ac0bc4102ffa16bc9b6
mahajrod <mahajrod@gmail.com>
2018-09-12 05:59:44 +0300

    pic

--- scripts/extract_snp_eff_info.py
+++ scripts/extract_snp_eff_info.py
@@ -10,8 +10,10 @@ parser.add_argument("-i", "--input_file", action="store", dest="input", required
                     help="Input vcf file with mutations.")
 parser.add_argument("-o", "--output_file", action="store", dest="output", required=True,
                     help="Output file with SNPeff info")
+parser.add_argument("-e", "--snpeff_entry", action="store", dest="snpeff_entry", default="ANN",
+                    help="SNPeff entry in vcf file. Default: ANN")
 
 args = parser.parse_args()
 
 mutations = CollectionVCF(in_file=args.input, from_file=True)
-mutations.extract_snpeff_info(args.output)
+mutations.extract_snpeff_info(args.output, snpeff_entry=args.snpeff_entry)
