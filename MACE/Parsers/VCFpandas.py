07d862112dd01de045ed169e31bde118c76768cd
mahajrod <mahajrod@gmail.com>
2019-02-28 22:06:14 -0500

    pic

--- MACE/Parsers/VCFpandas.py
+++ MACE/Parsers/VCFpandas.py
@@ -631,7 +631,9 @@ class CollectionVCF():
 
                 for parameter in uniq_format_dict[format_entry]:
                     print parameter
+
                     parameter_col = self.parse_column(tmp[parameter], parameter, "FORMAT")
+                    print parameter_col
                     sample_data_dict[sample][format_entry].append(parameter_col)
 
                 for i in range(0, len(uniq_format_dict[format_entry])):
