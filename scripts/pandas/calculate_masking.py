d9c54ff87b7250821c2ca0db6f4fb34383c387c6
mahajrod <mahajrod@gmail.com>
2019-03-03 17:51:19 -0500

    pic

--- scripts/pandas/calculate_masking.py
+++ scripts/pandas/calculate_masking.py
@@ -13,6 +13,11 @@ parser.add_argument("-i", "--input_file", action="store", dest="input", required
 parser.add_argument("-o", "--output", action="store", dest="output",
                     required=True,
                     help="Output file with masking")
+parser.add_argument("-c", "--sample_coverage", action="store", dest="sample_coverage",
+                    default=None, type=lambda s: map(float, s.split(",")),
+                    help="Comma-separated list of sample coverage to use, if not set median coverage"
+                         "calculated from vcf file will be used. Must have the same length as samples list"
+                         ". Default: not set")
 parser.add_argument("-s", "--sample_list", action="store", dest="sample_list",
                     default=None, type=lambda s: s.split(","),
                     help="Comma-separated list of samples to use for masking calculation. Default: all")
@@ -29,5 +34,6 @@ parser.add_argument("-u", "--min_coverage", action="store", dest="min_coverage",
 args = parser.parse_args()
 
 mutations = CollectionVCF(args.input, parsing_mode="pos_gt_dp")
-mutations.calculate_masking(args.output, samples=args.sample_list, min_samples=args.min_samples,
-                            max_coverage=args.max_coverage, min_coverage=args.min_coverage)
+mutations.calculate_masking(args.output, samples=args.sample_list, sample_coverage=args.sample_coverage,
+                            min_samples=args.min_samples, max_coverage=args.max_coverage,
+                            min_coverage=args.min_coverage)
