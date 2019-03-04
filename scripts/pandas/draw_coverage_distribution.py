f3d4ebf876f3a0008297574186ecc3cb3e57a723
mahajrod <mahajrod@gmail.com>
2019-03-03 23:56:02 -0500

    pic

--- scripts/pandas/draw_coverage_distribution.py
+++ scripts/pandas/draw_coverage_distribution.py
@@ -26,7 +26,8 @@ parser.add_argument("-e", "--output_formats", action="store", dest="output_forma
 parser.add_argument("-w", "--bin_width", action="store", dest="bin_width",
                     default=5, type=int,
                     help="Bin width for distribution histogram. Default: 5")
-
+parser.add_argument("--verbose", action="store_true", dest="verbose",
+                    help="Verbose stdout")
 """
 parser.add_argument("-a", "--scaffold_white_list", action="store", dest="scaffold_white_list", default=[],
                     type=lambda s: s.split(","),
@@ -40,4 +41,4 @@ args = parser.parse_args()
 mutations = CollectionVCF(args.input, parsing_mode="pos_gt_dp")
 mutations.get_coverage_distribution(args.output_prefix,
                                     bin_width=args.bin_width, dpi=args.dpi, subplot_size=3,
-                                    extension_list=args.output_formats)
+                                    extension_list=args.output_formats, verbose=args.verbose)
