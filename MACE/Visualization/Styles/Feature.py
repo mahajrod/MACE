8a1387dc32e0430a690320fd816aefd9cef750a1
mahajrod <mahajrod@gmail.com>
2019-09-04 22:03:21 +0700

    pic

---
+++ MACE/Visualization/Styles/Feature.py
@@ -0,0 +1,13 @@
+
+
+class FeatureStyle():
+
+    def __init__(self, patch_type, fill=True, edge_color=None, face_color=None, edge_width=None):
+        self.patch_type = patch_type
+        self.fill = fill
+        self.edge_color = edge_color
+        self.face_color = face_color
+        self.edge_width = edge_width
+
+
+default_feature_style = FeatureStyle(patch_type="rectangle")
