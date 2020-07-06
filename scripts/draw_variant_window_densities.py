8ac2486c5dabeae8c0e686d0cba4616c179ca236
mahajrod <zmeenosez72>
2020-07-06 20:50:11 +0300

    pic

--- scripts/draw_variant_window_densities.py
+++ scripts/draw_variant_window_densities.py
@@ -147,14 +147,16 @@ if args.coverage:
                                                       args.coverage_column_name),
                              index_col=(args.scaffold_column_name, args.window_column_name),
                              sep="\t")
+    scaffold_to_keep = StatsVCF.get_filtered_entry_list(masking_df.index.get_level_values(level=0).unique().to_list(),
+                                                        entry_white_list=args.scaffold_white_list)
+    print(scaffold_to_keep)
     if chr_syn_dict:
         masking_df.rename(index=chr_syn_dict, inplace=True)
     print("aaaaaaaa")
     print(masking_df)
 
-    scaffold_to_keep = StatsVCF.get_filtered_entry_list(masking_df.index.get_level_values(level=0).unique().to_list(),
-                                                        entry_white_list=args.scaffold_white_list)
-    print(scaffold_to_keep)
+
+
     masking_df = masking_df[masking_df.index.isin(scaffold_to_keep, level=0)]
 
     min_threshold = args.mean_coverage * args.min_coverage_threshold
