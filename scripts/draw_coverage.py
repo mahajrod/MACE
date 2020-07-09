1cb2f8ba512b7fdb65e0f7cc2a6eccedd3dc77e7
mahajrod <zmeenosez72>
2020-07-09 15:34:09 +0300

    pic

--- scripts/draw_coverage.py
+++ scripts/draw_coverage.py
@@ -111,7 +111,7 @@ if args.scaffold_syn_file:
 
 print(chr_syn_dict)
 print(coverage_df)
-
+print(coverage_df.index)
 Visualization.draw_coverage_windows(coverage_df, args.window_size, args.window_step, chr_len_df,
                                     args.mean_coverage,
                                     args.output_prefix,
