cec97cdc9cb6aa05b08d06840f024439247dec61
mahajrod <zmeenosez72>
2020-10-05 07:26:49 +0300

    pic

--- MACE/Visualization/Legends.py
+++ MACE/Visualization/Legends.py
@@ -87,6 +87,67 @@ class DensityLegend(Legend):
                                      xytext=(self.x_start, square_y_pos+ self.element_size*0.25), )
 
 
+class CoverageLegend(Legend):
+
+    def __init__(self, y_start=0, x_start=0, x_size=10, element_size=10, style=default_legend_style,
+                 colormap=None, thresholds=np.array((0.0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 2.5)),
+                 colors=("#333a97", "green", "yellow", "orange", "red"), background="white",
+                 masked="grey", fontsize=13):
+
+        Legend.__init__(self, y_start=y_start, x_start=x_start, x_size=x_size, element_size=element_size,
+                        style=style, colormap=colormap, fontsize=fontsize)
+
+        self.colors = colors
+        self.thresholds = thresholds
+
+        if colormap:
+            self.cmap = plt.get_cmap(self.colormap, len(self.thresholds))
+            self.colors = [self.cmap(i) for i in range(0, len(thresholds))]
+
+        self.background = background
+        self.masked = masked
+
+    def draw(self, axes=None, style=None):
+
+        used_style = style if style else self.style
+
+        current_subplot = axes if axes else plt.gca()
+
+        square_y_pos = self.y_start - self.element_size
+
+        for color, legend_label in zip((self.masked, self.background), ("masked",)):
+            square_y_pos += self.element_size
+            #print (self.x_start, square_y_pos), self.x_size, self.element_size, color
+            fragment = Rectangle((self.x_start - 2 * self.x_size, square_y_pos), self.x_size, self.element_size,
+                                 fill=True,
+                                 edgecolor="black", facecolor=color, linewidth=0.5)
+
+            current_subplot.add_patch(fragment)
+
+            current_subplot.annotate(legend_label,
+                                     xy=(self.x_start, square_y_pos + self.element_size*0.25), xycoords='data',
+                                     fontsize=self.fontsize,
+                                     xytext=(self.x_start, square_y_pos+ self.element_size*0.25), )
+
+        for i in range(0, len(self.thresholds)):
+            square_y_pos += self.element_size
+            # print (colormap_tuple_list[i][1])
+            fragment = Rectangle((self.x_start - 2 * self.x_size, square_y_pos), self.x_size, self.element_size,
+                                 fill=True,
+                                 edgecolor="black", facecolor=self.colors[i], linewidth=0.5)
+
+            current_subplot.add_patch(fragment)
+            if i == (len(self.thresholds) - 1):
+                legend_element_label = "> %.2fx" % self.thresholds[i]
+            else:
+                legend_element_label = "%.2fx - %.2fx" % (self.thresholds[i], self.thresholds[i + 1])
+
+            current_subplot.annotate(legend_element_label,
+                                     xy=(self.x_start, square_y_pos+ self.element_size*0.25), xycoords='data',
+                                     fontsize=self.fontsize,
+                                     xytext=(self.x_start, square_y_pos+ self.element_size*0.25), )
+
+
 class FeatureLegend(Legend):
 
     def __init__(self, y_start=0, x_start=0, x_size=10, element_size=10, style=default_legend_style,
