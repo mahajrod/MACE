eef27ffeabb2f4687baa7e3513d0bcdef2cc19ea
mahajrod <zmeenosez72>
2020-11-05 20:03:44 +0300

    pic

--- scripts/draw_variant_window_densities.py
+++ scripts/draw_variant_window_densities.py
@@ -173,7 +173,6 @@ if not args.only_count:
         #print(count_df)
         count_df.to_csv("%s.variant_counts.with_masking.tsv" % args.output_prefix, sep='\t', header=True, index=True)
 
-
     Visualization.draw_variant_window_densities(count_df, args.window_size, args.window_step, chr_len_df,
                                                 args.output_prefix,
                                                 figure_width=15,
