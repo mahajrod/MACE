008fe843e2abf28b2d530810c9d4b2a646a8a897
mahajrod <mahajrod@gmail.com>
2023-04-23 20:49:29 +0200

    fixed wrong legend in draw_coverage.py.

--- scripts/STR/convert_STR_vcf_to_allel_length.py
+++ scripts/STR/convert_STR_vcf_to_allel_length.py
@@ -23,15 +23,34 @@ parser.add_argument("--amplicon_len_column_name", action="store", dest="amplicon
                     default="amplicon_len",
                     help="Column name  in amplicon length file (--amplicon_len_file) to be used as amplicon length."
                          "Ignored if --amplicon_len_file is not set.")
-
+parser.add_argument("--pop_file", action="store", dest="pop_file",
+                    help="Two column tab-separated file. First column should contain sample ids, "
+                         "second - population ids. Optional. Ignored if --add_population_column is not set")
+parser.add_argument("--add_population_column", action="store_true", dest="add_population_column",
+                    help="Add column with population ids to the output. "
+                         " If --pop_file was set, population ids will be extracted from the corresponding file."
+                         " Otherwise, each sample will be assigned to a separated population. ")
+parser.add_argument("--encode_ids", action="store_true", dest="encode_ids",
+                    help="Encode sample and population ids as numbers")
 parser.add_argument("--na_replacement", action="store", dest="na_replacement", default="-100",
                     help="Value to use in output file to replace NA. Default: '-100'")
 parser.add_argument("-o", "--output", action="store", dest="output", default=sys.stdout,
-                    help="Output_file with length of STR allels.")
+                    help="Output file with length of STR allels.")
+parser.add_argument("-p", "--pop_df_file", action="store", dest="pop_df_file",
+                    help="File to save encoded sample and pop ids. Default: not set")
 
 args = parser.parse_args()
 
 str_vcf_col = CollectionVCF(in_file=args.input, parsing_mode="complete")
+
+if args.pop_file:
+    pop_df = pd.read_csv(args.pop_file, sep="\t", header=None, index_col=0, names=["id", "pop_id"])
+else:
+    pop_df = pd.DataFrame.from_records(zip(str_vcf_col.samples,
+                                           range(1, len(str_vcf_col.samples) + 1)), index=0, columns=["id", "pop_id"])
+
+pop_df["id_code"] = range(1, len(pop_df) + 1)
+pop_df["pop_id_code"] = range(1, len(pop_df) + 1)
 
 if args.amplicon_len_file:
     str_len_df = pd.read_csv(args.amplicon_len_file, sep="\t", header=0, usecols=(args.amplicon_id_column_name,
@@ -67,4 +86,19 @@ new_genotype_df = pd.DataFrame.from_records(genotype_length_array,
 
 allel_df = new_genotype_df.transpose().droplevel(level=1)
 allel_df.index.name = "sample"
+
+if args.encode_ids and args.prefix:
+    pop_df.to_csv(args.pop_df_file, sep="\t", header=True, index=True)
+
+if args.encode_ids and args.add_population_column:
+    allel_df["pop_id"] = pop_df["pop_id_code"]
+elif args.add_population_column:
+    allel_df["pop_id"] = pop_df["pop_id"]
+
+if args.add_population_column:  # make pop_id column first
+    allel_df = allel_df[[allel_df.columns[-1]] + allel_df.columns[:-1]]
+
+if args.encode_ids:
+    allel_df.rename(index=pop_df["id_code"], inplace=True)
+
 allel_df.to_csv(args.output, sep="\t", index=True, na_rep=args.na_replacement)
